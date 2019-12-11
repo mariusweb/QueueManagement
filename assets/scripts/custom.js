c27fe688a80f3e7f4b37bd319b5f7e4c0f27b324
mariusweb <39734577+mariusweb@users.noreply.github.com>
2019-12-11 16:45:00 +0200

    Adding JSON file

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,29 +1,56 @@
 (function(){
 
 "use strict";
-	
+
 		let primaryButton = document.querySelector(".btn-primary");
+
+		// On clickind 
 		primaryButton.addEventListener("click", function(e){
 		e.preventDefault();
+
+		// Showe number
 		let showPrimary = document.querySelector(".primary-number");
 		let show = showPrimary.style.display='flex';
+
+		// Hide everything accept number
 		let hideContainer = document.querySelector(".register-container");
 		let hide = hideContainer.style.display='none';
+
+		// Count numbers
 		let counter = document.querySelector(".primary-number");
 	 	let textArray = counter.innerHTML.split(" ");
 	 	textArray[1] = textArray[1] * 1 + 1;
+
+	 	// Count numbers until 99 and start over from 1
 	 	if(textArray[1] >= 100){
 	 		textArray[1] = 0;
 	 	}
-		 	let newCounter = counter.innerHTML = textArray.join(" ");
-		 	localStorage.setItem('key', textArray[1]);
-		 	let takingBack = document.querySelector(".taking-back");
-		 	takingBack
+		counter.innerHTML = textArray.join(" ");
+		// Setting timer for showing number
 		setTimeout(function () {
-			let showPrimary = document.querySelector(".primary-number").style.display='none';
+			const showPrimary = document.querySelector(".primary-number").style.display='none';
 			let hideContainer = document.querySelector(".register-container").style.display='flex';
-		}, 3000);
+		}, 2000);
+
+
+		// LocalStorage
+		//let showScoreboard = document.querySelector(".taking-back");
+		 	
+		localStorage.setItem("key1", textArray[1]);
+
+		let getForTakingBack = localStorage.getItem("key1");
+
+		// JSON 
+
+		// xhr.open("GET", chrome.extention.getURL("../json/clients.json"), true);
+		// xhr.send();
+
+
 		});
+
+
+
+
 		
 
 
@@ -44,7 +71,7 @@
 		setTimeout(function () {
 			let showSecondary = document.querySelector(".secondary-number").style.display='none';
 			let hideContainer = document.querySelector(".register-container").style.display='flex';
-		}, 3000);
+		}, 2000);
 		});
 
 
@@ -65,7 +92,7 @@
 		setTimeout(function () {
 			let showInfo = document.querySelector(".info").style.display='none';
 			let hideContainer = document.querySelector(".register-container").style.display='flex';
-		}, 3000);
+		}, 2000);
 		});
 		
 	
