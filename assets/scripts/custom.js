1513c77412c699b0ffff6fb2fbc09e1d92d4c6bb
mariusweb <39734577+mariusweb@users.noreply.github.com>
2020-02-17 16:56:49 +0200

    Ajusted forms
    Forms input value is added into localStorage.

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -7,50 +7,56 @@
 			localStorage.setItem('data', dataStringify);
 			let dataParse = localStorage.getItem('data');
 			let takeBack = JSON.parse(dataParse);
-			
 			console.log(takeBack.takingBack);
-			
-
 		});
 
-		document.querySelector(".btn-outline-primary").addEventListener("click", function(e){
+		let takingBack = [];
+		
+		const addTakingBack = (e) =>{
 			e.preventDefault();
-				// Showe number
-				document.querySelector(".primary-number").style.display='flex';
-				
-	
-				// Hide everything accept number
-				document.querySelector(".register-container").style.display='none';
-				document.querySelector(".take-back").style.display='none';
-	
-				// Count numbers
-				let counter = document.querySelector(".primary-number");
-				let textArray = counter.innerHTML.split(" ");
-				textArray[0] = textArray[0] * 1 + 1;
-	
-				// Count numbers until 99 and start over from 1
-				if(textArray[0] >= 100){
-					textArray[0] = 0;
-				}
-				counter.innerHTML = textArray.join(" ");
-				// Setting timer for showing number
-				setTimeout(function () {
-					document.querySelector(".primary-number").style.display='none';
-					document.querySelector(".register-container").style.display='flex';
-				}, 2000);
-
-
-		});
+			// Showe number
+			document.querySelector(".primary-number").style.display='flex';
+			// Hide everything accept number
+			document.querySelector(".register-container").style.display='none';
+			document.querySelector(".take-back").style.display='none';
+			// Count numbers
+			let counter = document.querySelector(".primary-number");
+			let textArray = counter.innerHTML.split(" ");
+			textArray[1] = textArray[1] * 1 + 1;
+			// Count numbers until 99 and start over from 1
+			if(textArray[1] >= 100){
+				textArray[1] = 0;
+			}
+			counter.innerHTML = textArray.join(" ");
+			//Adding client in client object
+			let client = {
+				firstName: document.querySelector(".take-fname").value,
+				lastName: document.querySelector(".take-lname").value,
+				id: textArray[1]
+			}
+			takingBack.push(client);
+			document.querySelector('.take-back').reset();
+			//Adding clients into localStorage
+			localStorage.setItem('TakingBackList', JSON.stringify(takingBack) );
+			// Setting timer for showing number
+			setTimeout(function () {
+				document.querySelector(".primary-number").style.display='none';
+				document.querySelector(".register-container").style.display='flex';
+			}, 2000);
+		}
+		document.addEventListener('DOMContentLoaded', ()=>{
+			document.querySelector(".btn-outline-primary").addEventListener("click", addTakingBack);
+		})
 		document.querySelector(".btn-primary").addEventListener("click", function(e){
 			e.preventDefault();
 			document.querySelector(".take-back").style.display='flex';
-
 			document.querySelector(".register-container").style.display='none';
-
 		});
 
 		
 
+		
+
 
 		
 		
@@ -68,9 +74,8 @@
 		
 		
 		
-		
-		
-		document.querySelector(".btn-outline-success").addEventListener("click", function(e){
+		let turningIn = [];
+		const addTurningIn = (e) => {
 			e.preventDefault();
 			document.querySelector(".secondary-number").style.display='flex';
 			document.querySelector(".register-container").style.display='none';
@@ -78,16 +83,28 @@
 			
 			let counter = document.querySelector(".secondary-number");
 			let textArray = counter.innerHTML.split(" ");
-			textArray[0] = textArray[0] * 1 + 1;
-			if(textArray[0] >= 200){
-				textArray[0] = 100;
+			textArray[1] = textArray[1] * 1 + 1;
+			if(textArray[1] >= 200){
+				textArray[1] = 100;
 			}
 			counter.innerHTML = textArray.join(" ");
+			let clientTurn = {
+				firstName: document.querySelector(".turn-fname").value,
+				lastName: document.querySelector(".turn-lname").value,
+				id: textArray[1]
+			}
+			turningIn.push(clientTurn);
+			document.querySelector('.turn-in').reset();
+			//Adding clients into localStorage
+			localStorage.setItem('TurningInList', JSON.stringify(turningIn) );
 			setTimeout(function () {
 				document.querySelector(".secondary-number").style.display='none';
 				document.querySelector(".register-container").style.display='flex';
 			}, 2000);
-		});
+		}
+		document.addEventListener('DOMContentLoaded', ()=>{
+			document.querySelector(".btn-outline-success").addEventListener("click", addTurningIn);
+		})
 
 		document.querySelector(".btn-success").addEventListener("click", function(e){
 			e.preventDefault();
@@ -111,30 +128,41 @@
 
 
 
+		let infoClientList = [];
+		const addInfoClient = (e) => {
+		
+			e.preventDefault();
+			document.querySelector(".info").style.display='flex';
+			document.querySelector(".register-container").style.display='none';
+			document.querySelector(".info-form").style.display='none';
 
-
-		document.querySelector(".btn-outline-info").addEventListener("click", function(e){
-		e.preventDefault();
-		document.querySelector(".info").style.display='flex';
-		document.querySelector(".register-container").style.display='none';
-		document.querySelector(".info-form").style.display='none';
-
-		let counter = document.querySelector(".info");
-	 	let textArray = counter.innerHTML.split(" ");
-	 	textArray[0] = textArray[0] * 1 + 1;
-	 	if(textArray[0] >= 300){
-	 		textArray[0] = 200;
-	 	}
-	 	counter.innerHTML = textArray.join(" ");
-		setTimeout(function () {
-			document.querySelector(".info").style.display='none';
-			document.querySelector(".register-container").style.display='flex';
-		}, 2000);
-		});
+			let counter = document.querySelector(".info");
+			let textArray = counter.innerHTML.split(" ");
+			textArray[1] = textArray[1] * 1 + 1;
+			if(textArray[1] >= 300){
+				textArray[1] = 200;
+			}
+			counter.innerHTML = textArray.join(" ");
+			let clientInfo = {
+				firstName: document.querySelector(".info-fname").value,
+				lastName: document.querySelector(".info-lname").value,
+				id: textArray[1]
+			}
+			infoClientList.push(clientInfo);
+			document.querySelector('.info-form').reset();
+			localStorage.setItem('InfoClientList', JSON.stringify(infoClientList) );
+			setTimeout(function () {
+				document.querySelector(".info").style.display='none';
+				document.querySelector(".register-container").style.display='flex';
+			}, 2000);
+		}
+		document.addEventListener('DOMContentLoaded', ()=>{
+			document.querySelector(".btn-outline-info").addEventListener("click", addInfoClient);
+		})
 		document.querySelector(".btn-info").addEventListener("click", function(e){
 			e.preventDefault();
 
-			document.querySelector(".turn-in").style.display='flex';
+			document.querySelector(".info-form").style.display='flex';
 
 			document.querySelector(".register-container").style.display='none';
 
